0ef587b40cf364d476ff2909db872a23698cd43a
joshski <joshuachisholm@gmail.com>
2011-01-06 17:48:41 +0000

    bump version to 0.1.2

--- lib/lackie.rb
+++ lib/lackie.rb
@@ -2,7 +2,7 @@ lib = File.dirname(__FILE__)
 $:.unshift(lib) unless $:.include?(lib) || $:.include?(File.expand_path(lib))
 
 module Lackie
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
 end
 
 require 'lackie/remote_control'

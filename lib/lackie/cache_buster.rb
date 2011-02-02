6715f6f2eaa423e2b5ac8795c7526bfa7f962968
joshski <joshuachisholm@gmail.com>
2011-02-02 14:34:38 +0000

    cache busting that actually works

--- lib/lackie/cache_buster.rb
+++ lib/lackie/cache_buster.rb
@@ -2,7 +2,7 @@ module Lackie
   class CacheBuster
     def self.unique_string
       @@unique_number ||= 0
-      (@@unique_number += 1).to_s
+      "#{@@unique_number += 1}#{Time.now.to_i}"
     end
   end
 end
--- spec/lackie/cache_buster_spec.rb
+++ spec/lackie/cache_buster_spec.rb
@@ -3,8 +3,13 @@ require 'lackie/cache_buster'
 
 module Lackie
   describe CacheBuster do
-    it "returns unique strings" do
+    it "produces unique strings" do
       (1..99).map { |i| CacheBuster.unique_string }.uniq.size.should == 99
+    end
+    
+    it "produces strings that include the current time" do
+      Time.should_receive(:now).and_return("332211")
+      CacheBuster.unique_string.should =~ /332211/
     end
   end
 end
